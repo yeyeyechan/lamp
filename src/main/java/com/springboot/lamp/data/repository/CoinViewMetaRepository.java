e40c27501346dec0663391b09211e7007eb90dfb
yeyeyechan <xamevh123@gmail.com>
2023-08-07 00:37:05 +0900

    join 문,union문 추가

--- src/main/java/com/springboot/lamp/data/repository/CoinViewMetaRepository.java
+++ src/main/java/com/springboot/lamp/data/repository/CoinViewMetaRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CoinViewMetaRepository extends JpaRepository<CoinViewMeta, String> {
 
-  @Query(value= "INSERT INTO coin_view_meta (market, korean_name,english_name,  market_warning,logo, id,name,slug,symbol) "
-      +" (SELECT a.market, a.korean_name, a.english_name, a.market_warning,b.logo, b.id,b.name,b.slug,b.symbol FROM upbit_market a "+
-      "INNER JOIN coin_market_meta b "+
-      "ON a.english_name=b.name "+
-      "WHERE a.market LIKE 'KRW-%')", nativeQuery = true)
+  @Query(value= "REPLACE INTO coin_view_meta (market, korean_name,english_name,  market_warning,logo, id,name,slug,symbol) "
+      +"(SELECT totalresult.market, totalresult.korean_name, totalresult.english_name, totalresult.market_warning,totalresult.logo, totalresult.id,totalresult.name,totalresult.slug, totalresult.symbol "+
+      "FROM (SELECT result1.market, result1.korean_name, result1.english_name, result1.market_warning,result1.logo, result1.id,result1.name,result1.slug, result1.symbol FROM (SELECT a.market, a.korean_name,"+
+      "a.english_name, a.market_warning,b.logo, b.id,b.name,b.slug, b.symbol FROM upbit_market a INNER JOIN coin_market_meta b ON a.english_name=b.name) as result1 "+
+      "UNION ALL "
+      +"SELECT result2.market, result2.korean_name, result2.english_name, result2.market_warning,result2.logo, result2.id,result2.name,result2.slug, result2.symbol FROM (SELECT a.market, a.korean_name, a.english_name,"
+      +"a.market_warning,b.logo, b.id,b.name,b.slug, b.symbol FROM upbit_market a INNER JOIN coin_market_meta b ON (SELECT SUBSTRING_INDEX(a.market,\"KRW-\",-1))=b.symbol) as result2  ) as totalresult WHERE "
+      +"totalresult.market LIKE 'KRW-%' GROUP BY totalresult.korean_name)", nativeQuery = true)
   List<CoinViewMeta> makeCoinViewMeta();
   CoinViewMeta findByMarket(String market);
 
